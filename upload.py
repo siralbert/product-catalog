aa6934ca5572b892dd5c62ece3a6b28f1839b8c5
Albert <albert.tsai21@gmail.com>
2024-03-26 16:11:27 -0500

    Update upload.py
    Add UPLOAD_IMAGES_DIR variable to upload.py

--- upload.py
+++ upload.py
@@ -5,11 +5,13 @@ from flask import abort, make_response, request
 from werkzeug.utils import secure_filename
 import os
 
+UPLOAD_IMAGES_DIR="/var/www/product-catalog/static/images"
+
 def upload():
     if 'image' in request.files:
         file = request.files['image']
         filename = secure_filename(file.filename)
-        file.save(os.path.join("/var/www/product-catalog/static/images",filename))
+        file.save(os.path.join(UPLOAD_IMAGES_DIR,filename))
         return 'File uploaded successfully'
 
     return 201
